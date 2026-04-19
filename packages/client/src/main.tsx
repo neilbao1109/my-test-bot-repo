bac90b14b3af9ca7490568b641c7df8e956d6ea0
Ubuntu <azureuser@openclaw-vm1.5zyy1tbkmswuvms2ukneurbpvg.rx.internal.cloudapp.net>
2026-04-19 03:25:13 +0000

    fix: add Object.hasOwn polyfill for iOS 15 Safari

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,3 +1,8 @@
+// Polyfill for iOS 15 (Safari <15.4)
+if (!(Object as any).hasOwn) {
+  (Object as any).hasOwn = (obj: any, prop: PropertyKey) => Object.prototype.hasOwnProperty.call(obj, prop);
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
